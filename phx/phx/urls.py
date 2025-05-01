e33ff6b6c338dabcc84094c93078454310cbc139
lachiemurray <lachie.murray@gmail.com>
2025-05-01 20:37:51 +0100

    Fix lint

--- phx/phx/urls.py
+++ phx/phx/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.views.static import serve
 from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls.static import static
 from django.urls import path, re_path
+from django.views.static import serve
 from error.views import Error400View, Error403View, Error404View, error_500
 
 from .admin import phx_admin
